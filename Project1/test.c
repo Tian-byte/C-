8e3bb6327e65c385ec644e112b28a9cc6d5d01d2
田园佳 <@LAPTOP-8VDA5MEQ>
2022-08-14 12:22:41 +0800

    添加项目文件。

---
+++ Project1/test.c
@@ -0,0 +1,67 @@
+//#include<stdio.h>
+//int main()
+//{
+//    double arr[5];
+//    double avg=0;
+//    int j;
+//    {
+//        for (j = 0; j < 5; j++)
+//        {
+//            scanf_s("%.2lf", &arr[j]);
+//            avg += arr[j];
+//        }
+//        printf("%.2lf", avg/ 5);
+//    }
+//    return 0;
+//    
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int j=0;
+//	for (int i = 1; i <= 100; i++)
+//	{
+//		if (i%10== 0)
+//		{
+//			j++;
+//		}
+//		if (i / 10==9)
+//			j++;
+//	}
+//	printf("%d",j);
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int i;
+//int main()
+//{
+//	i--;
+//	if (i > sizeof(i))
+//		printf("<");
+//	else
+//		printf(">");
+//
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
